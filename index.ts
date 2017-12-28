128ec2395c2bb47c0b4b92ef5699f1dc046265ce
Evan White <kindasimple@gmail.com>
2017-12-28 14:05:20 -0500

    handle empty inputs

--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ import { getHand } from './suits'
 
 // handle parameters passed from commandline and output spiral
 if (require.main === module) {
-    const result = getHand(process.argv[2])
-    console.log(result)
+    let cards = process.argv[2]
+    if(cards) {
+        const result = getHand(process.argv[2])
+        console.log(result)
+    }
 }
 
